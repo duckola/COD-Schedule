5cb6c0c906932559433f124354b8b68b84ecbaab
duckola <leejasmin.adolfo@cit.edu>
2025-09-16 16:09:04 +0800

    Deploy React

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Replace <username> and <repo-name> with your GitHub info
 export default defineConfig({
+  base: '/COD-Schedule/',
   plugins: [react()],
-})
+});
